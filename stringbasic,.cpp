e3113abfdc3ecb503c41670fb109adf3680f368c
anupreeya24 <anupreeyawo@gmail.com>
2024-08-14 11:23:02 +0530

    Create stringbasic,.cpp

---
+++ stringbasic,.cpp
@@ -0,0 +1,26 @@
+//Anupreeya Bhattacharjee
+//23070123022
+
+//string basic
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str1[4]="c++";
+    char str2[]={'c','+','+','\0'};
+    char str3[]={'c','+','+','\0'};
+    char str4[]="c++";
+    
+    cout<<"string 1:"<<str1<<endl;
+    cout<<"string 2:"<<str2<<endl;
+    cout<<"string 3:"<<str3<<endl;
+    cout<<"string 4:"<<str4<<endl;
+
+    return 0;
+}
+
+//string 1:c++
+//string 2:c++
+//string 3:c++
+//string 4:c++
