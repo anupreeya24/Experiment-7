c4171cdfed59fcf33d80ce3eb1c8160ff082e5d6
anupreeya24 <anupreeyawo@gmail.com>
2024-08-14 11:14:36 +0530

    Create arraymin.cpp

---
+++ arraymin.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, Min;
+    int n=5; 
+    int arr[] = {10, 30, 20, 50, 5};
+
+    Min = arr[0];
+
+    for (i = 1; i < n; i++) { 
+        if (arr[i] < Min) { 
+            Min = arr[i]; 
+        }
+    }
+
+    cout << "The minimum value is: " << Min << endl;
+
+    return 0;
+}
+
+// The minimum value is: 5
