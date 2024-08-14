10897d2f5ef078476d2dffe75dfd60f4c6c18ae4
anupreeya24 <anupreeyawo@gmail.com>
2024-08-14 11:37:49 +0530

    Create concatenation.cpp

---
+++ concatenation.cpp
@@ -0,0 +1,16 @@
+//Anupreeya Bhattacharjee
+//23070123022
+//string (a)conctenation 
+#include <iostream>
+using namespace std;
+int main() {
+   string firstName = "Anupreeya ";
+   string lastName = "Bhattacharjee";
+   string fullName = firstName + lastName;
+   cout << fullName;
+    
+  
+    return 0;
+}
+
+//Anupreeya Bhattacharjee
