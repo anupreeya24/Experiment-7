26ff1238bf3bff72b5fb13f2a5c35c4f48e75f35
anupreeya24 <anupreeyawo@gmail.com>
2024-08-14 11:44:00 +0530

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,20 @@
+//Anupreeya Bhattacharjee
+//23070123022
+//string (b)Reverse string
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str = "COMPUTER";
+    int n = str.length();  
+
+    
+    for (int i = n - 1; i >= 0; i--) {
+        cout << str[i];
+    }
+    cout << endl;
+
+    return 0;
+}
+
+//RETUPMOC
